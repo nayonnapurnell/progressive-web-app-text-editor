edc9adff666402c54a1b3ef89aec56fc9015f5d5
“Nayonna <Nayonna Purnell>
2023-02-07 22:02:25 -0500

    Adding code reviewed project

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -22,28 +19,30 @@ module.exports = () => {
         template: './index.html',
         title: 'J.A.T.E'
       }),
-
        // Injects our custom service worker
        new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-
-      new InjectManifest(),
       new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
         name: 'Just Another Text Editor',
         short_name: 'JATE',
         description: 'Just Another Text Editor',
         background_color: '#ffffff',
+        theme_color: '#ffffff',
+        start_url: './',
         publicPath: './',
         crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
           }
         ]
-      }),
-     
+      }),    
     ],
 
     module: {
